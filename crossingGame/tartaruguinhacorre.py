977d5c92b0b06cdc1c06ddbd3320af2404cfde62
Larissa Sá <160503350+larimsa@users.noreply.github.com>
2024-05-16 17:53:20 -0300

    Create tartaruguinhacorre.py

---
+++ crossingGame/tartaruguinhacorre.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+is_race_on = False
+screen = turtle.Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Faça sua aposta", prompt="Qual tartaruguita vai ganhar? escolha sua cor (red, orange, yellow, green, blue, purple): ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for turtle_index in range(0, 6):
+    new_turtle = turtle.Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"Você ganhou! A tartaruguita {winning_color} é a vencedora")
+            else:
+                print(f"Você perdeu! A tartaruguita {winning_color} é a vencedora!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
